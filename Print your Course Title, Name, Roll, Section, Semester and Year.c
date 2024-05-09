2352a58b7c2da9eb0aaec8c97f51156e1503f24b
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-05-09 22:03:06 +0600

    lab

---
+++ Print your Course Title, Name, Roll, Section, Semester and Year.c
@@ -0,0 +1,14 @@
+//Print your Course Title, Name, Roll, Section, Semester and Year.
+
+
+#include <stdio.h>
+int main()
+{
+ printf("Course Title: Structured Programming Language\n");
+ printf("Name: Akash Paul\n");
+ printf("Roll: 21078\n");
+ printf("Section: B\n");
+ printf("Semester: 1st\n");
+ printf("Year: 1st\n");
+ return 0;
+}
